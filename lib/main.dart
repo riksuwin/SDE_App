e70207128e4adb358d15770c1dfbbe33d602977d
riksuwin <layasan.rexowen@gmail.com>
2023-10-04 12:20:14 +0800

    login choice for personnel & resident

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:final_app/loginPage/login_page_personnel.dart';
 import 'package:final_app/loginSignupPage/scratch.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MyApp());
